b525f418db811b243dbb6dfbab867dba5935dc9c
Rachaud Keyes <rachaudkeyes@gmail.com>
2025-03-16 14:27:22 -0500

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,18 @@
+
+import { render, screen, waitFor } from '@testing-library/react';
+import App from './App';
+import axios from "axios"
+import dogs from "./_testCommon"
+
+jest.mock('axios');
+
+test('renders App', async () => {
+  axios.get.mockResolvedValue({ data: dogs });
+  const { container } = render(<App />);
+  const heading = await waitFor(() => screen.findByText('Welcome!'));
+  const nav = container.querySelector("nav")
+  expect(nav).toBeInTheDocument()
+
+  expect(heading).toBeInTheDocument();
+  expect(axios.get).toHaveBeenCalled()
+});
